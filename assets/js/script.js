d4e6870c2e60cbaa5eead296e4c0127991b739da
matteo granata <matteogranata492@gmail.com>
2023-12-04 18:48:37 +0100

    counter function created and reset confirmation button added

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,3 +59,72 @@ securityContainer.appendChild(confirmButton);
 
 const deleteButton = createElement('button', "delete", "CANCEL");
 securityContainer.appendChild(deleteButton);
+
+
+//contatore
+let number = 0
+
+//increase function
+buttonIncrement.addEventListener("click", function () {
+  number++;
+  result.innerHTML = number;
+  result.style.fontSize = `${10}vh`;
+
+  result.animate({
+    fontSize: `${12}vh`
+  },
+    { duration: 200, fill: "forwards" });
+
+  result.animate({
+    fontSize: `${10}vh`,
+  }, { duration: 200, fill: "forwards" });
+
+  if (number % 10 == 0) {
+    notice.innerHTML = `Complimenti sei arrivato a ${number}`;
+  };
+  confirmButton.style.display = 'none'
+  deleteButton.style.display = 'none'
+});
+
+//decrease function
+buttonDecrement.onclick = function () {
+  number--;
+  result.innerHTML = number;
+
+  result.style.fontSize = `${10}vh`;
+  result.animate({
+    fontSize: `${12}vh`,
+  }, { duration: 200, fill: "forwards" });
+
+  result.animate({
+    fontSize: `${10}vh`,
+  }, { duration: 200, fill: "forwards" });
+
+  if (number % 10 == 0) {
+    notice.innerHTML = `Complimenti sei arrivato a ${number}`;
+  };
+
+  confirmButton.style.display = 'none'
+  deleteButton.style.display = 'none'
+};
+
+//reset function
+buttonReset.onclick = function () {
+  deleteButton.style.display = 'block'
+  confirmButton.style.display = 'block'
+  notice.innerHTML = `SEI SICURO?`
+
+  confirmButton.onclick = function () {
+    number = 0;
+    result.innerHTML = number;
+
+    confirmButton.style.display = 'none'
+    deleteButton.style.display = 'none'
+    notice.innerHTML = `Contatore azzerato`
+  };
+  deleteButton.onclick = function () {
+    confirmButton.style.display = 'none'
+    deleteButton.style.display = 'none'
+    notice.innerHTML = `azione annullata`
+  };
+}
